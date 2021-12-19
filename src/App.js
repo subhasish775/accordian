e960957fae6b10a02aa4b11d3462bd1f41d86a87
Subhasish Ghosh <93440758+subhasish775@users.noreply.github.com>
2021-12-19 12:51:49 +0530

    accordian

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import data from './data';
+import Question from './Question';
+// import './App.css';
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+
+  return <main>
+ <div className="container">
+   <h3>questions and ansewer about login</h3>
+   <section className="info">
+   {
+    questions.map((question)=>{
+      return <Question key={question.id} {...question}/>
+    })
+
+   }
+
+
+   </section>
+ </div>
+
+  
+</main>
+
+
+
+  
+
+    
+  
+}
+
+export default App;
